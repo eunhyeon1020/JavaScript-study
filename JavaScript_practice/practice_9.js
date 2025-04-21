e9989c9149c1ff712d70bc55c8e28f66c1fefc78
eunhyeon <eunhyeon1734@gmail.com>
2025-04-21 13:15:06 +0900

    자바스크립트 100제 9~10번 문제 완 - 10번 문제 해석 필요

--- JavaScript_practice/practice_9.js
+++ JavaScript_practice/practice_9.js
@@ -9,10 +9,14 @@ var hour = '11';
 var minute = '34';
 var second = '27';
 
-var result = 
+var result = year.concat("/", month.concat("/", day.concat
+  (" ", hour.concat(":", minute.concat(":", second)))));
 
-console.log(result);
-
+console.log(result)
 
 // 출력 값 
 // 2019/04/26 11:34:27
+
+// ** 답 : var result = year.concat('/', month, '/', day, ' ', hour,  ':', minute, ':', second) ** 
+// str.concat(string1, string2, ..., stringN)
+// : 매개변수로 전달된 모든 문자열을 호출 문자열에 붙인 새로운 문자열을 반환 
