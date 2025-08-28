b2facc751c5eda9278942ea6d48dc907a4caaf46
eunhyeon <eunhyeon1734@gmail.com>
2025-08-29 02:52:39 +0900

    미니 프로젝트 - 게시판 리스트 기능 구현 1. html  변경

--- 제로초_js_study/mini-project/script/board.js
+++ 제로초_js_study/mini-project/script/board.js
@@ -1,14 +1,18 @@
+// 리스트 만드는 로직
 const boardList = document.querySelector(".board-list");
 
 function renderBoardList(dateKey) {
   boardList.innerHTML = "";
 
+  // memos = 배열
   const memos = memoData[dateKey] || [];
+  // forEach(각 회의록 객체, 인덱스 번호)
   memos.forEach((m, i) => {
     const item = document.createElement("div");
+    item.classList.add("memoList");
     item.innerHTML = `
-      <strong>${m.topic}</strong> (${m.attendees})<br/>
-      <p>${m.content}</p>
+        <div class="listIndex">${i + 1}. </div> 
+        <div class="listTopic">${m.topic}</div> 
       <hr/>
     `;
     boardList.appendChild(item);
